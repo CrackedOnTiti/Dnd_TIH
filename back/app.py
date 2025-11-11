7dce7a043d72fdc7dbf750673e8b2992d2d1adf6
CrackedOnTiti <thierry.bungaroo@epitech.eu>
2025-11-11 19:01:46 +0100

    [ADD] basic socket testing

--- back/app.py
+++ back/app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+from flask_socketio import SocketIO, emit
+import os
 
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+FRONT_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'front')
 
 @app.route('/')
-def hello():
-    return "Hello World"
+def index():
+    return "Go to /host or /player"
+
+@app.route('/host')
+def host():
+    return send_from_directory(FRONT_DIR, 'host.html')
+
+@app.route('/player')
+def player():
+    return send_from_directory(FRONT_DIR, 'player.html')
+
+@socketio.on('increment')
+def handle_increment():
+    print('Player clicked increment!')
+    emit('increment', broadcast=True)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
