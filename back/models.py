a28124dde10a7c6283638aadd851dbc6e39978ea
CrackedOnTiti <thierry.bungaroo@epitech.eu>
2025-11-11 20:58:30 +0100

    [UPD] adding new models for new character create

--- back/models.py
+++ back/models.py
@@ -5,6 +5,10 @@ class Player(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     player_name = db.Column(db.String(100), nullable=False)
+    power = db.Column(db.String(100), nullable=False)
+    power_description = db.Column(db.Text, nullable=False)
+    sex = db.Column(db.String(20), nullable=False)
+    physical_description = db.Column(db.Text, nullable=False)
     curr_hp = db.Column(db.Integer, default=20)
     max_hp = db.Column(db.Integer, default=20)
     last_dice_roll = db.Column(db.Integer, default=0)
@@ -13,6 +17,10 @@ class Player(db.Model):
         return {
             'id': self.id,
             'player_name': self.player_name,
+            'power': self.power,
+            'power_description': self.power_description,
+            'sex': self.sex,
+            'physical_description': self.physical_description,
             'curr_hp': self.curr_hp,
             'max_hp': self.max_hp,
             'last_dice_roll': self.last_dice_roll
